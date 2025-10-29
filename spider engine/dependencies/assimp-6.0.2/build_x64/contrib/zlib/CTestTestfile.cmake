dcacfc67d2b28a72b9853afbca9e1b474ba2a70c
K4LL <k4ll.569lk@gmail.com>
2025-10-29 14:48:07 -0300

    Fix DXC

--- spider engine/dependencies/assimp-6.0.2/build_x64/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/gupue/source/repos/spider engine/spider engine/dependencies/assimp-6.0.2/contrib/zlib
-# Build directory: C:/Users/gupue/source/repos/spider engine/spider engine/dependencies/assimp-6.0.2/build_x64/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
